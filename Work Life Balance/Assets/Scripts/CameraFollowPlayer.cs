379b6037ae33d16ae351903705b4c4110789483c
michaeltranxd <mtt2@uic.edu>
2020-03-23 23:32:32 -0500

    Added movement and camera(one axis only though)
    Hopefully this is good enough movement and camera movement.
    Camera will move around player using the mouse and player will move according to the direction of the camera.

---
+++ Work Life Balance/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public float mouseSensitivity = 50f;
+
+    public Transform playerBody;
+    private Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cameraOffset = transform.position - playerBody.transform.position;
+    }
+
+    // LateUpdate called after Update methods
+    void LateUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        Quaternion camTurnAngle = Quaternion.AngleAxis(mouseX, Vector3.up);
+        cameraOffset = camTurnAngle * cameraOffset;
+
+        transform.position = playerBody.transform.position + cameraOffset;
+        transform.LookAt(playerBody);
+    }
+
+}
---
+++ Work Life Balance/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 8f;            // Speed of player
+    public float gravity = -9.81f;      // Gravity constant for world
+
+    bool isGrounded;                    // Boolean value describing the groundness of the player
+
+    Vector3 velocity = new Vector3(0, 0, 0);                   // Velocity of the player
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+
+        if (!isGrounded)
+        {
+            // Apply gravity
+            velocity.y += gravity * Time.deltaTime;
+        }
+        if (isGrounded && velocity.y < 0)
+        {
+            // Character is grounded so we do not have velocity downwards (we are not moving!)
+            velocity.y = 0f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        // Movement Handling
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Apply movement relative to camera
+        Vector3 move = Camera.main.transform.right * x + Camera.main.transform.forward * z;
+
+        // Remove rotation up/down direction
+        move.y = 0f;
+
+        // Move controller
+        controller.Move(move * speed * Time.deltaTime);
+        // Apply gravity physics to player
+        controller.Move(velocity * Time.deltaTime);
+
+        // Only rotate when moving
+        if(move != Vector3.zero)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move.normalized), 0.1f);
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        print(hit);
+    }
+}
