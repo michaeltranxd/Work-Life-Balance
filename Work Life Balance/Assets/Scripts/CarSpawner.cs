8b4be14f1b8415806199b3979a8c2f70c47f1dca
michaeltranxd <mtt2@uic.edu>
2020-04-07 16:22:16 -0500

    Added car spawner

---
+++ Work Life Balance/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    float time = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        print(time);
+    }
+}
