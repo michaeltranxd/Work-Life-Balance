6dfaf32dac9c17b580604645cfad787c0896ddcf
michaeltranxd <mtt2@uic.edu>
2020-04-12 15:52:53 -0500

    Added minimap

--- Work Life Balance/Assets/Scripts/Minimap.cs
+++ Work Life Balance/Assets/Scripts/Minimap.cs
@@ -6,13 +6,16 @@ public class Minimap : MonoBehaviour
 {
     public Transform player;
     public Transform camera;
+    public Transform icon;
+    public Transform mapIcons;
 
     private Camera minimapCamera;
+    
 
     private float currentZoom;
     private float minZoom = 5;
     private float maxZoom = 50;
-    private float increments = 2;
+    private float increments = 1;
 
     void Start()
     {
@@ -21,13 +24,23 @@ public class Minimap : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadMinus) && minimapCamera.orthographicSize < maxZoom)
+        if (Input.GetKey(KeyCode.KeypadMinus) && minimapCamera.orthographicSize < maxZoom)
         {
             minimapCamera.orthographicSize = minimapCamera.orthographicSize + increments;
+            icon.transform.localScale = new Vector3(minimapCamera.orthographicSize, minimapCamera.orthographicSize, minimapCamera.orthographicSize);
+            foreach (Transform child in mapIcons)
+            {
+                child.transform.localScale = new Vector3(minimapCamera.orthographicSize, minimapCamera.orthographicSize, minimapCamera.orthographicSize);
+            }
         }    
-        if (Input.GetKeyDown(KeyCode.KeypadPlus) && minimapCamera.orthographicSize > minZoom)
+        if (Input.GetKey(KeyCode.KeypadPlus) && minimapCamera.orthographicSize > minZoom)
         {
             minimapCamera.orthographicSize = minimapCamera.orthographicSize - increments;
+            icon.transform.localScale = new Vector3(minimapCamera.orthographicSize, minimapCamera.orthographicSize, minimapCamera.orthographicSize);
+            foreach (Transform child in mapIcons)
+            {
+                child.transform.localScale = new Vector3(minimapCamera.orthographicSize, minimapCamera.orthographicSize, minimapCamera.orthographicSize);
+            }
         }
     }
     void LateUpdate()
