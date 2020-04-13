f4fd88e05006bd0f378e4f23cbca7f2f815fa024
michaeltranxd <mtt2@uic.edu>
2020-04-13 16:23:53 -0500

    Updated minimap + added map feature animations

---
+++ Work Life Balance/Assets/Scripts/Map.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool mapActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animator != null)
+        {
+            if (Input.GetKeyDown(KeyCode.M) && !mapActive)
+            {
+                animator.SetBool("mapOpen", true);
+                animator.SetBool("mapClose", false);
+                mapActive = true;
+            }
+            else if(Input.GetKeyDown(KeyCode.M) && mapActive)
+            {
+                animator.SetBool("mapClose", true);
+                animator.SetBool("mapOpen", false);
+                mapActive = false;
+            }
+        }        
+    }
+}
