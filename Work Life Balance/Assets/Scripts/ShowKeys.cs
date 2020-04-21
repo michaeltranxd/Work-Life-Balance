321b4e6927c1a842ad3036d3071bed12df65891b
spancerrr <spancerguo@gmail.com>
2020-04-21 12:45:27 -0500

    added control screen

---
+++ Work Life Balance/Assets/Scripts/ShowKeys.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowKeys : MonoBehaviour
+{
+    public RectTransform controlPanel;
+    private bool keyActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //show control screen
+        if(keyActive == true && Input.GetKeyDown(KeyCode.H)){
+            controlPanel.gameObject.SetActive(false);
+            keyActive = false;
+        }
+        //close controk screen
+        else if(keyActive == false && Input.GetKeyDown(KeyCode.H)){
+            controlPanel.gameObject.SetActive(true);
+            keyActive = true;
+        } 
+    }
+}
