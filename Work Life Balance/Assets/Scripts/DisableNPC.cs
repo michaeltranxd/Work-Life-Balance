0742ad5335bf827c819c84b129dba0f8a102bfd8
spancerrr <spancerguo@gmail.com>
2020-04-25 14:29:47 -0500

    added NPCs

---
+++ Work Life Balance/Assets/Scripts/DisableNPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableNPC : MonoBehaviour
+{
+    public GameObject NPCs;
+    public DayNightController dayNightController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dayNightController.getCurrentHour() == 19 && dayNightController.getCurrentMinute() == 0){
+            NPCs.SetActive(false);
+        }
+        else if(dayNightController.getCurrentHour() == 9 && dayNightController.getCurrentMinute() == 0){
+            NPCs.SetActive(true);
+        }
+    }
+}
