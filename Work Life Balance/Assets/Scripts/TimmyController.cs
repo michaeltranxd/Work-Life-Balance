508614cf93493f90faeb0da58e5bf8a32280bc45
spancerrr <spancerguo@gmail.com>
2020-04-25 12:26:52 -0500

    fixed Timmy

--- Work Life Balance/Assets/Scripts/TimmyController.cs
+++ Work Life Balance/Assets/Scripts/TimmyController.cs
@@ -13,6 +13,7 @@ public class TimmyController : MonoBehaviour
 
     private int counter = 0;
     public GameObject player;
+    public GameObject npc;
 
     public AudioSource SoundManager;
     public AudioClip notificationSound;
@@ -28,20 +29,23 @@ public class TimmyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(player.transform.position,this.transform.position)< 1 && !inCoroutine)
-        {
-            audioSource.Play();
-            inCoroutine = true;
-            StartCoroutine(Wait());
-        }else{
-            if(counter > 1000){
-            transform.rotation *= Quaternion.Euler(0,90.0f, 0);
-            counter = 0;
+        if(!inCoroutine){
+            if(Vector3.Distance(player.transform.position,this.transform.position)< 1)
+            {
+                audioSource.Play();
+                inCoroutine = true;
+                inCoroutine = true;
+                StartCoroutine(Wait());
+            }else{
+                if(counter > 1000){
+                transform.rotation *= Quaternion.Euler(0,90.0f, 0);
+                counter = 0;
+                }
+                else{
+                    transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * 1.0f;
+                }
+                counter ++;
             }
-            else{
-                transform.position += transform.TransformDirection (Vector3.forward) * Time.deltaTime * 1.0f;
-            }
-            counter ++;
         }
     }
     IEnumerator Wait()
@@ -51,7 +55,7 @@ public class TimmyController : MonoBehaviour
         messagePlane.gameObject.SetActive(true);
         yield return new WaitForSeconds(3);
         messagePlane.gameObject.SetActive(false);
-        Destroy(gameObject);
+        Destroy(npc);
         StatsManager.AddNutri(10.0f);
         message.text = "";
     }
